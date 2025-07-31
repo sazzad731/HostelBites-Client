5c6c1e4c7b468f02e8dfa42af74b80c9dc389e7d
sazzad731 <sazzadho731@gmail.com>
2025-07-31 23:50:35 +0600

    Fix: prevent infinite loop in UpcomingMeals component

--- src/pages/UpcomingMeals/UpcomingMeals.jsx
+++ src/pages/UpcomingMeals/UpcomingMeals.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAxiosSecureOrPublic from "../../hooks/useAxiosSecureOrPublic";
 import { FaHeart, FaRegHeart } from "react-icons/fa6";
 import useLike from "../../hooks/useLike";
-import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import useDbUser from "../../hooks/useDbUser";
 
 const UpcomingMeals = () => {
-  const axiosSecure = useAxiosSecure();
+  const {axiosSecure} = useAxiosSecureOrPublic();
   const { user } = useAuth();
   const { handleLike, like } = useLike();
-  const [likedMeal, setLikedMeal] = useState([])
+  const { badge } = useDbUser();
 
   const { data: upcomingMeals = [] } = useQuery({
     queryKey: ["getUpcomingMeals", like],
@@ -19,10 +19,9 @@ const UpcomingMeals = () => {
     },
   });
 
-  useEffect(()=>{
-    const likedMeal = upcomingMeals.filter(meal => meal.likes.includes(user?.email))
-    setLikedMeal(likedMeal)
-  }, [upcomingMeals, user?.email])
+ const likedMeal = upcomingMeals.filter((meal) =>
+   meal.likes.includes(user?.email)
+ );
 
   return (
     <div className="min-h-screen">
@@ -52,23 +51,45 @@ const UpcomingMeals = () => {
                 {meal.description}
               </p>
             </div>
-            <button
-              onClick={() => handleLike(meal._id)}
-              disabled={likedMeal.some((liked) => liked._id === meal._id)}
-              className="flex gap-2 cursor-pointer items-center justify-center rounded-lg px-4 py-2 flex-row-reverse bg-[#f3e7e8] text-[#1b0e0e] font-medium leading-normal w-fit disabled:cursor-not-allowed"
-            >
-              {likedMeal.some((liked) => liked._id === meal._id) ? (
-                <>
-                  Liked
-                  <FaHeart size={25} />
-                </>
-              ) : (
-                <>
-                  Like
-                  <FaRegHeart size={25} />
-                </>
+            <div className="flex gap-2">
+              <button
+                onClick={() => handleLike(meal._id)}
+                disabled={meal.likes.includes(user?.email)}
+                className="flex gap-2 cursor-pointer items-center justify-center rounded-lg px-4 py-2 flex-row-reverse bg-[#f3e7e8] text-[#1b0e0e] font-medium leading-normal w-fit disabled:cursor-not-allowed"
+              >
+                {likedMeal.some((liked) => liked._id === meal._id) ? (
+                  <>
+                    Liked
+                    <FaHeart size={25} />
+                  </>
+                ) : (
+                  <>
+                    Like
+                    <FaRegHeart size={25} />
+                  </>
+                )}
+              </button>
+              {badge === "Bronze" && (
+                <div className="flex items-center gap-2 text-sm text-stone-500">
+                  <svg
+                    className="text-primary"
+                    fill="none"
+                    height="16"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    width="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 15V9"></path>
+                    <path d="M12 7h.01"></path>
+                  </svg>
+                  <span className="font-medium">Premium Only</span>
+                </div>
               )}
-            </button>
+            </div>
           </div>
           <div
             className="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-lg flex-1"
